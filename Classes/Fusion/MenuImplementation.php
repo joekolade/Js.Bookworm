704898f9d6901c94079317b69eb76f7cc502b6c7
Joekolade <joe@joekola.de>
2018-03-12 22:33:29 +0100

    try

--- Classes/Fusion/MenuImplementation.php
+++ Classes/Fusion/MenuImplementation.php
@@ -39,7 +39,7 @@ class MenuImplementation extends \Neos\Neos\Fusion\MenuImplementation
             'state' => self::STATE_NORMAL,
             'label' => $currentNode->getLabel(),
             'menuLevel' => $this->currentLevel,
-            'property' => $currentNode->getProperties(),
+            'menubadge' => $currentNode->getProperty('menubadge'),
         );
 
         $item['state'] = $this->calculateItemState($currentNode);
